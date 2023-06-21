786459bf666821ca39621b0572b3247547f74635
simrett <leesim242@gmail.com>
2023-06-21 14:46:33 -0300

    functions prototype for monty

---
+++ monty.h
@@ -0,0 +1,86 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#define Buffsize 30
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+ int n;
+ struct stack_s *prev;
+ struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+ char *opcode;
+ void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+
+typedef struct global_var
+{
+ FILE *file;
+ char *buff;
+ size_t tmp;
+ instruction_t *dict;
+ stack_t *head;
+ unsigned int line_number;
+ int MODE;
+} vars;
+
+extern vars var;
+
+/* ------------------ */
+/* main files */
+/* ------------------ */
+instruction_t *new_dict();
+int glob_vars(vars *var);
+int opcode_call(vars *var, char *opcode);
+void frees_all(void);
+int _isdigit(char *string);
+
+/* ------------------ */
+/* operations */
+/* ------------------ */
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
