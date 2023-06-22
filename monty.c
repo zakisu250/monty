3e2665e7c3d0f298e9793cbd13f40f0b7b02d65e
simrett <leesim242@gmail.com>
2023-06-22 22:26:19 +0300

    fix betty

--- monty.c
+++ monty.c
@@ -3,11 +3,11 @@
 bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ * main - monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
+ * Return: 0 on success
+ */
 int main(int argc, char **argv)
 {
 	char *content;
