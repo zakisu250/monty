e0bd3ca9cce4cfda5458bea9587bd1f930918068
simrett <leesim242@gmail.com>
2023-06-22 19:52:12 +0300

    spelling fix

--- monty.h
+++ monty.h
@@ -73,7 +73,7 @@ void swap(stack_t **head, unsigned int counter);
 void add(stack_t **head, unsigned int counter);
 void nop(stack_t **head, unsigned int counter);
 void sub(stack_t **head, unsigned int counter);
-void div(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
 void mul(stack_t **head, unsigned int counter);
 
 /* ========================================== */
