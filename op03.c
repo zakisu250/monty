ac97d3a0934e0aea20ec12f0916fe4c9b26f51cc
zakisu250 <zakisu250@gmail.com>
2023-06-22 21:14:39 +0300

    removed multiple definition

--- op03.c
+++ op03.c
@@ -66,39 +66,3 @@ void addnode(stack_t **head, int n)
 	*head = new_node;
 }
 
-/**
- * addqueue - add node to the tail stack
- * @n: new node data
- * @head: head of the stack
- * Return: void
- */
-void addqueue(stack_t **head, int n)
-{
-	stack_t *new_node, *tmp;
-
-	tmp = *head;
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		printf("Error\n");
-	}
-
-	new_node->n = n;
-	new_node->next = NULL;
-
-	if (tmp)
-	{
-		while (tmp->next)
-			tmp = tmp->next;
-	}
-	if (!tmp)
-	{
-		*head = new_node;
-		new_node->prev = NULL;
-	}
-	else
-	{
-		tmp->next = new_node;
-		new_node->prev = tmp;
-	}
-}
